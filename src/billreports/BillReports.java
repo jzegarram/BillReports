092bcb282411b03b9d95d581b2b98ff2034b7816
Jose Zegarra <jzegarram2000@gmail.com>
2023-09-14 22:08:19 -0500

    adding a comment about shamir

--- src/billreports/BillReports.java
+++ src/billreports/BillReports.java
@@ -16,6 +16,7 @@ public class BillReports {
     public static void main(String[] args) {
         // TODO code application logic here
         // comment 1
+        // Shamir regalon
     }
     
 }
